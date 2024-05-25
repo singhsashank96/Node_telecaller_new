2594406153b2dc622dd6de148fef39e6db47a448
SashankSingh97 <129739180+SashankSingh97@users.noreply.github.com>
2024-05-25 17:34:19 +0530

    v-6 chnage

--- src/Utlis/sequelize.js
+++ src/Utlis/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 // Define common Sequelize instance
-const sequelize = new Sequelize('u553948927_telecaller', 'u553948927_telecaller', 'Izequal2@tech', {
+const sequelize = new Sequelize('u553948927_tele_hosting', 'u553948927_tele_hosting', 'IzEqual2@tech', {
     host: '217.21.87.205',
     dialect: 'mysql'
   });
